31f501682d076a92f09822d41a36609b7b5c6427
tahaozturkk <ozturktaha1453@gmail.com>
2022-10-22 23:45:47 +0300

    first commit

---
+++ DataAccessLayer/Repositories/CommentRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CommentRepository : IGenericDal<Comment> 
+    {
+        Context c = new();
+        public void Delete(Comment t)
+        {
+            c.Comments.Remove(t);
+            c.SaveChanges();
+        }
+
+        public Comment GetById(int id)
+        {
+            return c.Comments.Find(id);
+        }
+
+        public List<Comment> GetListAll()
+        {
+            return c.Comments.ToList();
+        }
+
+        public void Insert(Comment t)
+        {
+            c.Comments.Add(t);
+            c.SaveChanges();
+        }
+
+        public void Update(Comment t)
+        {
+            c.Comments.Update(t);
+            c.SaveChanges();
+        }
+    }
+}
